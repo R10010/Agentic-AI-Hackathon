a31f49bd70b80f2e3b82c5bfffcf5d15bd13d959
Ren <147902427+R10010@users.noreply.github.com>
2026-05-24 15:56:49 +0530

    update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
  
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.0-flash')
 else:
     print(" WARNING: GEMINI_API_KEY not found in .env. Running in Fallback Mode.")
     model = None
